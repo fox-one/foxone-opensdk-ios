42804939d7ff48d2341f955038e60fec6e8d1f16
duyiping <duyp@fox.one>
2019-02-16 14:04:45 +0800

    update framework

---
+++ F1Utility/F1Passport.framework/Headers/F1Passport.h
@@ -0,0 +1,19 @@
+//
+//  F1Passport.h
+//  F1Passport
+//
+//  Created by  on 2019/1/23.
+
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for F1Passport.
+FOUNDATION_EXPORT double F1PassportVersionNumber;
+
+//! Project version string for F1Passport.
+FOUNDATION_EXPORT const unsigned char F1PassportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <F1Passport/PublicHeader.h>
+
+
